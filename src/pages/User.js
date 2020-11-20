98693b2d1fd936c298dc66d8222963a685541b67
Salaki na Komaki <komaki4lyfe@gmail.com>
2020-11-21 00:13:39 +0700

    user change password and username

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Modal, Button, Alert } from 'react-bootstrap';
 
 class User extends React.Component {
     state = {
@@ -7,47 +8,18 @@ class User extends React.Component {
             username: '',
             id: 0
         },
-        username: '',
-        password: '',
-        newPassword: '',
-        error: ''
+        error: '',
+        modalUsername: false,
+        modalPassword: false
     }
 
-    handleUsername = (e) => {
-        this.setState({ username: e.target.value });
-    }
-
-    handlePassword = (e) => {
-        this.setState({ password: e.target.value });
-    }
-
-    handleNewPassword = (e) => {
-        this.setState({ newPassword: e.target.value });
-    }
-
-    updateUsername = async () => {
-        const params = {
-            username: this.state.currentUser.username,
-            password: this.state.password,
-            new_username: this.state.username
-        }
-
-        try {
-            console.log("params => ", params);
-            const updateRequest = await axios.post('http://localhost:3000/user/updateUsername', params);
-            console.log(updateRequest.data);
-            this.setState({ error: '' });
-        }
-        
-        catch(err) {
-            console.log(err);
-            this.setState({ error: err.response.data.message });
-        }
+    loadUserData() {
+        const currentUser = JSON.parse(localStorage.getItem("user"));
+        this.setState({ currentUser });
     }
 
     componentDidMount() {
-        const currentUser = JSON.parse(localStorage.getItem("user"));
-        this.setState({ currentUser });
+        this.loadUserData();
     }
 
     render() {
@@ -63,22 +35,175 @@ class User extends React.Component {
                         </pre>
                     </div>
                 </div>
-                <div className="form-group d-flex" style={{ gap: 10 }}>
-                    <div style={{ width: 400 }}>
-                        <input type="text" className="form-control" name="username" placeholder="New Username" value={this.state.username} onChange={this.handleUsername}/>
-                    </div>
-                    <button className="btn btn-primary" onClick={() => this.updateUsername()}>Update Username</button>
-                </div>
-                <div className="form-group d-flex" style={{ gap: 10 }}>
-                    <div style={{ width: 195 }}>
-                        <input type="password" className="form-control" name="password" placeholder="Old Password" value={this.state.password} onChange={this.handlePassword}/>
-                    </div>
-                    <div style={{ width: 195 }}>
-                        <input type="password" className="form-control" name="new_password" placeholder="New Password" value={this.state.newPassword} onChange={this.handleNewPassword}/>
-                    </div>
-                    <button className="btn btn-primary">Update Password</button>
-                </div>
+                <Button
+                    variant="primary"
+                    onClick={() => this.setState({ modalUsername: true })}
+                    className="mr-2">
+                    <i className="fa fa-user mr-1"></i>
+                    Change Username
+                </Button>
+                <Button
+                    variant="primary"
+                    onClick={() => this.setState({ modalPassword: true })}>
+                    <i className="fa fa-key mr-1"></i>
+                    Change Password
+                </Button>
+                <Modal show={this.state.modalUsername} onHide={() => this.setState({ modalUsername: false })}>
+                    <ModalChangeUsername
+                        onClose={() => this.setState({ modalUsername: false })}
+                        onUpdate={() => this.loadUserData()}>
+                    </ModalChangeUsername>
+                </Modal>
+                <Modal show={this.state.modalPassword} onHide={() => this.setState({ modalPassword: false })}>
+                    <ModalChangePassword
+                        onClose={() => this.setState({ modalPassword: false })}>
+                    </ModalChangePassword>
+                </Modal>
             </div>
+        )
+    }
+}
+
+class ModalChangeUsername extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    state = {
+        username: '',
+        password: '',
+        alertSuccess: false,
+        alertError: false
+    }
+
+    handleUsername = (e) => this.setState({ username: e.target.value });
+    handlePassword = (e) => this.setState({ password: e.target.value });
+    changeUsername = async () => {
+        const currentUser = JSON.parse(localStorage.getItem("user"));
+        const params = {
+            username: currentUser.username,
+            password: this.state.password,
+            new_username: this.state.username
+        }
+
+        try {
+            const updateRequest = await axios.post('http://localhost:3000/user/updateUsername', params);
+            localStorage.setItem('user', JSON.stringify(updateRequest.data.data));
+            this.setState({ alertSuccess: true, alertError: false });
+            this.props.onUpdate();
+        }
+        catch(err) {
+            this.setState({ alertError: true, alertSuccess: false });
+            console.log(err);
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Modal.Header closeButton>
+                    <Modal.Title>Change Username</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="form-group">
+                        <label htmlFor="username">New Username</label>
+                        <input className="form-control" name="username" placeholder="New Username" autoComplete="off" onChange={this.handleUsername}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input className="form-control" type="password" name="password" placeholder="Password" onChange={this.handlePassword}/>
+                    </div>
+                    <Alert show={this.state.alertSuccess} variant="success" onClose={() => this.setState({ alertSuccess: false })} dismissible>
+                        <Alert.Heading>Success!</Alert.Heading>
+                        Success updating username!
+                    </Alert>
+                    <Alert show={this.state.alertError} variant="danger" onClose={() => this.setState({ alertError: false })} dismissible>
+                        <Alert.Heading>Error!</Alert.Heading>
+                        Failed updating username!
+                    </Alert>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="light" onClick={this.props.onClose}>Cancel</Button>
+                    <Button variant="primary" onClick={() => this.changeUsername()}>Save</Button>
+                </Modal.Footer>
+            </>
+        )
+    }
+}
+
+class ModalChangePassword extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        password: '',
+        newPassword: '',
+        newPasswordConfirm: '',
+        alertSuccess: false,
+        alertError: ''
+    }
+
+    handlePassword = (e) => this.setState({ password: e.target.value });
+    handleNewPassword = (e) => this.setState({ newPassword: e.target.value });
+    handleNewPasswordConfirm = (e) => this.setState({ newPasswordConfirm: e.target.value });
+    
+    changePassword = async () => {
+        if (this.state.newPassword != this.state.newPasswordConfirm) {
+            this.setState({ alertError: 'New password mismatch!' });
+            return;
+        }
+
+        const currentUser = JSON.parse(localStorage.getItem("user"));
+        const params = {
+            username: currentUser.username,
+            password: this.state.password,
+            new_password: this.state.newPassword,
+        }
+
+        try {
+            const updateRequests = await axios.post('http://localhost:3000/user/updatePassword', params);
+            this.setState({ alertSuccess: true, alertError: "" });
+        }
+        catch(err) {
+            this.setState({ alertError: "Failed updating password!", alertSuccess: false });
+            console.log(err);
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Modal.Header closeButton>
+                    <Modal.Title>Change Password</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="form-group">
+                        <label htmlFor="password">Current Password</label>
+                        <input type="password" className="form-control" name="password" placeholder="Current Password" onChange={this.handlePassword}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="new-password">New Password</label>
+                        <input type="password" className="form-control" name="new-password" placeholder="New Password" onChange={this.handleNewPassword}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="new-password-confirm">Confirm New Password</label>
+                        <input type="password" className="form-control" name="new-password-confirm" placeholder="Confirm New Password" onChange={this.handleNewPasswordConfirm}/>
+                    </div>
+                    <Alert show={this.state.alertSuccess} variant="success" onClose={() => this.setState({ alertSuccess: false })} dismissible>
+                        <Alert.Heading>Success!</Alert.Heading>
+                        Success updating password!
+                    </Alert>
+                    <Alert show={this.state.alertError != ''} variant="danger" onClose={() => this.setState({ alertError: false })} dismissible>
+                        <Alert.Heading>Error!</Alert.Heading>
+                        { this.state.alertError }
+                    </Alert>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="light" onClick={() => this.props.onClose()}>Cancel</Button>
+                    <Button variant="primary" onClick={() => this.changePassword()}>Save</Button>
+                </Modal.Footer>
+            </>
         )
     }
 }
