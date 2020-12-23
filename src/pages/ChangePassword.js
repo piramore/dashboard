073cec8c6bda2887b558405d972a9a6f59f5e0b0
Salaki na Komaki <komaki4lyfe@gmail.com>
2020-12-23 20:05:01 +0700

    commit

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import { AppService } from '../services/app.service';
 import PasswordIllust from '../assets/images/password.svg';
+import { Spinner, Alert } from 'react-bootstrap';
 
 class ChangePassword extends React.Component {
     constructor(props) {
@@ -16,6 +17,9 @@ class ChangePassword extends React.Component {
         // alerts
         alertSuccess: '',
         alertError: '',
+
+        // loading
+        changePasswordLoading: false,
     }
 
     handlePassword = e => this.setState({ password: e.target.value });
@@ -26,11 +30,13 @@ class ChangePassword extends React.Component {
 
         if (this.state.password !== this.state.passwordRe) return;
 
+        this.setState({ changePasswordLoading: true });
         this.appService.changePassword(this.state.password).then(
             response => {
                 this.setState({
                     alertSuccess: "Update password success!",
-                    alertError: ""
+                    alertError: "",
+                    changePasswordLoading: false
                 });
             }
         ).catch(
@@ -42,7 +48,8 @@ class ChangePassword extends React.Component {
                 else message = 'Failed updating password.';
                 this.setState({
                     alertSuccess: '',
-                    alertError: message
+                    alertError: message || 'Failed to change password.',
+                    changePasswordLoading: false
                 })
             }
         )
@@ -77,10 +84,29 @@ class ChangePassword extends React.Component {
                             />
                         </div>
                         <div>
-                            <button className="btn btn-primary">Submit</button>
+                            <button
+                                className="btn btn-primary"
+                                disabled={this.state.changePasswordLoading}
+                                onClick={() => this.changePassword()}>
+                                {
+                                    this.state.changePasswordLoading ?
+                                    <Spinner animation='border' as="span" role="status" size="sm" /> :
+                                    "Submit"
+                                }
+                            </button>
                         </div>
                     </div>
                     <img src={PasswordIllust} style={{ width: 400 }}/>
+                </div>
+                <div className="mt-4">
+                    <Alert show={this.state.alertError !== ''} variant="danger" onClose={() => this.setState({ alertError: '' })}>
+                        <Alert.Heading>Error!</Alert.Heading>
+                        { this.state.alertError }
+                    </Alert>
+                    <Alert show={this.state.alertSuccess !== ''} variant="success" onClose={() => this.setState({ alertSuccess: '' })}>
+                        <Alert.Heading>Success!</Alert.Heading>x
+                        { this.state.alertSuccess }
+                    </Alert>
                 </div>
                 <Sidebar/>
             </>
