a005279a45735795470d1a84db72fa45b472cf45
Salaki na Komaki <komaki4lyfe@gmail.com>
2020-12-24 21:15:14 +0700

    fix reset password redirect notyf

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Alert, Button, Spinner } from 'react-bootstrap';
 import { AppService } from '../services/app.service';
+import { Notyf } from 'notyf';
 
 class ResetPassword extends React.Component {
     constructor(props) {
         super(props);
         this.appService = new AppService();
+        this.notyf = new Notyf();
     }
 
     state = {
+
+        // params
         token: '',
         password: '',
         passwordRe: '',
-        alertError: '',
-        alertSuccess: '',
+
+        // state
         loading: false,
         emailSent: false
     }
@@ -23,39 +27,31 @@ class ResetPassword extends React.Component {
     handlePasswordRe = (e) => this.setState({ passwordRe: e.target.value });
     resetPassword = async () => {
         if (!this.state.password || !this.state.passwordRe) {
-            this.setState({ alertError: 'Please fill password!' });
+            this.notyf.error("Please fill all required field!");
             return;
         }
 
         if (this.state.password !== this.state.passwordRe) {
-            this.setState({ alertError: 'Password mismatch.' });
+            this.notyf.error("Password missmatch!");
             return;
         }
 
+        this.setState({ loading: true });
         this.appService.resetPassword(this.state.token, this.state.password).then(
             response => {
                 if (response.data.success) {
-                    this.setState({
-                        alertSuccess: response.data.message,
-                        alertError: ''
-                    });
+                    this.setState({ loading: false });
+                    this.notyf.success(response.data.message);
+                    window.location.href = '/login';
                 }
-
-                else {
-                    this.setState({
-                        alertSuccess: '',
-                        alertError: response.data.message
-                    });
-                }
+                
+                else throw(response.data.message);
             }
         ).catch(
             error => {
                 this.setState({ loading: false });
-
-                let message;
-                if (error.response) message = error.response.data.message;
-                else message = 'Failed to update password';
-                this.setState({ alertSuccess: '', alertError: message });
+                if (typeof error == 'string') this.notyf.error(error);
+                else this.notyf.error('Failed to update password');
             }
         )
     }
@@ -68,42 +64,26 @@ class ResetPassword extends React.Component {
     render() {
         return(
             <div className="d-flex align-items-center justify-content-center bg-light" style={{ height: '100vh' }}>
-                {
-                    this.state.alertSuccess === '' ?
-                    <div className="border border-darken-2 rounded p-4 bg-white" style={{ width: 400 }}>
-                        <div className="font-weight-bold text-center text-dark mb-4" style={{ fontSize: '2rem' }}>Reset Password</div>
-                        <div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" name="password" placeholder="New Password" onChange={this.handlePassword}/>
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" name="passwordRe" placeholder="Confirm New Password" onChange={this.handlePasswordRe}/>
-                            </div>
-                            <Button variant="primary" block onClick={() => this.resetPassword()}>
-                                { this.state.loading ?
-                                    <Spinner animation='border' as='span' size="sm"/> :
-                                    <span>Submit</span>
-                                }
-                            </Button>
-                            <div className="mt-2 text-right">
-                                <Link to="/login" className="text-primary">Back to login</Link>
-                            </div>
-                            <div className="mt-4">
-                                <Alert show={this.state.alertError !== ''} variant="danger" onClose={() => this.setState({ alertError: '' })} dismissible>
-                                    <Alert.Heading>Error!</Alert.Heading>
-                                    { this.state.alertError }
-                                </Alert>
-                            </div>
+                <div className="border border-darken-2 rounded p-4 bg-white" style={{ width: 400 }}>
+                    <div className="font-weight-bold text-center text-dark mb-4" style={{ fontSize: '2rem' }}>Reset Password</div>
+                    <form id="formResetPassword" onSubmit={e => e.preventDefault()}>
+                        <div className="form-group">
+                            <input type="password" className="form-control" name="password" placeholder="New Password" onChange={this.handlePassword}/>
                         </div>
-                    </div> :
-
-                    <div className="text-center">
-                        <div className="text-dark" style={{ fontSize: '2rem' }}>
-                            { this.state.alertSuccess }
+                        <div className="form-group">
+                            <input type="password" className="form-control" name="passwordRe" placeholder="Confirm New Password" onChange={this.handlePasswordRe}/>
                         </div>
-                        <Link to="/login" className="text-primary">Back to login page</Link>
-                    </div>
-                }
+                        <Button variant="primary" type="submit" disabled={this.state.loading} block onClick={() => this.resetPassword()}>
+                            { this.state.loading ?
+                                <Spinner animation='border' as='span' size="sm"/> :
+                                <span>Submit</span>
+                            }
+                        </Button>
+                        <div className="mt-2 text-right">
+                            <Link to="/login" className="text-primary">Back to login</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
